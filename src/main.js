3fa3f8b2061c05747422df5e17ee6dd95accf080
le-gris <solimlegris@gmail.com>
2024-02-28 02:58:26 -0500

    vite setup with env variables

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { initializeApp } from 'firebase/app';
+import App from './App.vue';
+import firebaseConfig from './config';
+import { VueFire, VueFireAuth } from 'vuefire';
+import VueGtag from 'vue-gtag'; // google analytics
+// import router from './router';
+import './style.css';
 
-createApp(App).mount('#app')
+const firebaseApp = initializeApp({ firebaseConfig });
+const app = createApp(App);
+
+app.use(VueGtag, {
+  disableScriptLoad: import.meta.env.MODE === 'development', // disable script load in development
+  pageTrackerExcludedRoutes: ['config'], // ignore the config route
+  config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS },
+});
+
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+    // VueFireAuth(),
+  ],
+});
+
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, `${process.cwd()}/env/`, ''),
+    ...loadEnv('deploy', `${process.cwd()}/env/`, ''),
+    ...loadEnv('git', `${process.cwd()}/env/`, ''),
+  };
+  return defineConfig({
+    plugins: [vue()],
+  });
+};
