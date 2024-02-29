a148438ebc326b71df6fa3a8ce14acc151676dfa
le-gris <solimlegris@gmail.com>
2024-02-29 03:00:33 -0500

    v0 functional

---
+++ scripts/upload_sampled_replay_info.mjs
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import figlet from 'figlet';
+// import shell from 'shelljs'
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where,
+  doc,
+} from 'firebase/firestore';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+// import appconfig from '../src/config'
+
+let count = 0;
+
+const init = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync('SMILE.', {
+        font: 'big',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+      })
+    )
+  );
+  console.log(chalk.green('your data is almost here.'));
+};
+
+const askQuestions = () => {
+  const questions = [
+    {
+      type: 'input',
+      name: 'FILENAME',
+      message: 'What is the name of the file without extension?',
+      default: 'data',
+    },
+  ];
+  return inquirer.prompt(questions);
+};
+
+const addData = async (data, c) => {
+  const localenv = dotenv.config({ path: 'env/.env.local' });
+  const firebaseConfig = {
+    apiKey: localenv.parsed.VITE_FIREBASE_APIKEY,
+    authDomain: localenv.parsed.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: localenv.parsed.VITE_FIREBASE_PROJECTID,
+    storageBucket: localenv.parsed.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId:
+      localenv.parsed.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: localenv.parsed.VITE_FIREBASE_APPID,
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  data.forEach(async (d) => {
+    try {
+      const docRef = await addDoc(collection(db, c), d);
+      console.log('Document written with ID: ', docRef.id);
+      count += 1;
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  });
+};
+
+const success = (count, c) => {
+  console.log(
+    chalk.green(`${count} new documents have been created in ${c}}.`)
+  );
+};
+
+const run = async () => {
+  // show script introduction
+  init();
+  const env = dotenv.config({ path: 'env/.env.git.local' });
+  const c = 'replayDataInfo';
+
+  // ask questions
+  const { FILENAME } = await askQuestions();
+
+  // read json file
+  const path = `data/${FILENAME}.json`;
+  const data = JSON.parse(fs.readFileSync(path, 'utf8'));
+
+  // for each data, create document in collection and add data
+  await addData(data, c);
+
+  // show success message
+  success(count, c);
+};
+
+run();
--- src/components/ReplayPage.vue
+++ src/components/ReplayPage.vue
@@ -1,23 +1,234 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, watchEffect, computed } from 'vue'
+import { useFirestore, useFirebaseStorage, useStorageFileUrl } from 'vuefire'
+import { getDocs, collection, query, where, } from 'firebase/firestore'
 import { ref as storageRef } from 'firebase/storage';
-import { useFirebaseStorage, useStorageFileUrl } from 'vuefire'
 
-
+const db = useFirestore()
 const storage = useFirebaseStorage()
-const replayVideoRef = storageRef(storage, '4A6jkZeYjATvk5zfelLR_level5.mp4')
-const {
-    url,
-    refresh,
-} = useStorageFileUrl(replayVideoRef)
 
+const levels = ['level10', 'level20', 'level5', 'level22']
+const filters = reactive({
+    insight: 0,
+    level: "",
+    won: true
+})
 
+let videos = ref([])
+let sortedVideos = ref([])
+let paginatedVideos = ref([])
+
+const sortBy = ref('insight')
+const perPage = ref(4)
+const perPageOptions = [1, 2, 4, 8]
+const currentPage = ref(1)
+
+const fetchVideos = async () => {
+    console.log('fetchVideos', filters)
+    // check if level is null
+    const q = filters.level === "" ? query(collection(db, 'replayDataInfo'), where('insight', '>=', parseInt(filters.insight), where('won', '==', filters.won))) : query(collection(db, 'replayDataInfo'), where('insight', '>=', parseInt(filters.insight)), where('level', '==', filters.level), where('won', '==', filters.won))
+
+    const querySnapshot = await getDocs(q);
+    console.log('querySnapshot', querySnapshot)
+
+    videos.value = []
+    querySnapshot.forEach(doc => {
+        videos.value.push(doc.data())
+    })
+    console.log('videos', videos.value)
+}
+
+const fetchStorageUrls = async () => {
+    const promises = videos.value.map(async video => {
+        const sRef = storageRef(storage, video.filename)
+        const { url, refresh } = useStorageFileUrl(sRef)
+        return { ...video, url }
+    })
+    sortedVideos.value = await Promise.all(promises)
+}
+
+watchEffect(() => {
+    fetchVideos()
+})
+
+watchEffect(() => {
+    fetchStorageUrls()
+})
+
+const sortVideos = () => {
+    sortedVideos.value = videos.value.slice().sort((a, b) => {
+        if (sortBy.value === 'insight') {
+            return a.insight - b.insight
+        } else {
+            // return random values
+            return Math.random() - 0.5
+        }
+    })
+}
+
+watchEffect(() => {
+    sortVideos()
+})
+
+watchEffect(() => {
+    const startIndex = (currentPage.value - 1) * perPage.value
+    const endIndex = startIndex + perPage.value
+    paginatedVideos.value = sortedVideos.value.slice(startIndex, endIndex)
+})
+
+const totalPages = computed(() => Math.ceil(sortedVideos.value.length / perPage.value))
+
+const prevPage = () => {
+    if (currentPage.value > 1) {
+        currentPage.value--
+    }
+}
+
+const nextPage = () => {
+    if (currentPage.value < totalPages.value) {
+        currentPage.value++
+    }
+}
+
+const playbackSpeed = ref("1"); // Default playback speed
+const secondsFromEnd = ref(5); // Default seconds from the end
+const secondsOptions = [5, 10, 15, 20, "max"]; // Options for seconds from the end
+
+// Method to play all videos
+const playAllVideos = () => {
+    // Loop through paginatedVideos and play each video
+    // get all video elements and play them
+    var videoElements = document.getElementsByTagName("video");
+    for (var i = 0; i < videoElements.length; i++) {
+        videoElements[i].playbackRate = playbackSpeed.value;
+        videoElements[i].currentTime = videoElements[i].duration - secondsFromEnd.value;
+        videoElements[i].play();
+    }
+};
 </script>
 
 <template>
-    <video width="320" height="240" controls>
-        <source v-if="url" :src="url" type="video/mp4" />
-    </video>
-</template>
+    <div>
+        <div class="user-controls">
+            <label>Minimum insight reported:</label>
+            <select v-model="filters.insight">
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
 
-<style scoped></style> 
+            <label>Level:</label>
+            <select v-model="filters.level">
+                <option value="">All</option>
+                <option v-for="level in levels" :key="level" :value="level">{{ level }}</option>
+            </select>
+
+            <label>Win:</label>
+            <input type="checkbox" v-model="filters.won">
+
+            <!-- <label>Sort By:</label>
+            <select v-model="sortBy">
+                <option value="insight">Insight</option>
+                <option value="score">Random</option>
+            </select> -->
+
+            <label>Videos Per Page:</label>
+            <select v-model="perPage">
+                <option v-for="option in perPageOptions" :value="option">{{ option }}</option>
+            </select>
+        </div>
+
+        <!-- New user controls -->
+        <div class="additional-controls">
+            <!-- Play button -->
+            <button @click="playAllVideos">Play All</button>
+
+            <!-- Speed control -->
+            <label>Speed:</label>
+            <select v-model="playbackSpeed">
+                <option value="1">1x</option>
+                <option value="0.75">0.75x</option>
+                <option value="0.5">0.5x</option>
+            </select>
+
+            <!-- Seconds from the end dropdown -->
+            <label>Seconds from end:</label>
+            <select v-model="secondsFromEnd">
+                <option v-for="seconds in secondsOptions" :value="seconds">{{ seconds }}</option>
+            </select>
+        </div>
+
+
+        <div class="grid">
+            <div class="grid-item" v-for="video in paginatedVideos" :key="video.doc_id" :id="video.doc_id">
+                <video :src="video.url" controls width="100%">
+                    Your browser does not support the video tag.
+                </video>
+                <div>
+                    <p class="insight">Insight: {{ video.insight }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pagination">
+            <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+            <br>
+            <span>
+                <b>{{ currentPage }}</b> / <b>{{ totalPages }}</b>
+            </span>
+            <br>
+            <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+        </div>
+    </div>
+</template>
+  
+<style scoped>
+.user-controls {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f0f0f0;
+    color: #000;
+    margin-bottom: 20px;
+}
+
+.additional-controls {
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px;
+    background-color: #f0f0f0;
+    color: #000;
+    margin-bottom: 20px;
+}
+
+.insight {
+    color: crimson;
+}
+
+.user-controls label {
+    margin-right: 5px;
+}
+
+.grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(600px, 1fr));
+    grid-template-rows: repeat(auto-fill, minmax(400px, 1fr));
+    gap: 20px;
+}
+
+.grid-item {
+    margin: 0;
+}
+
+.pagination {
+    margin-top: 20px;
+    text-align: center;
+    color: #000;
+}
+</style>
+
+  
